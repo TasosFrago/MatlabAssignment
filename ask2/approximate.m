d5d6e10c23c97e372c611661dfa5339dddc66025
TasosFrago <tasosfragkopoulos03@gmail.com>
2023-01-19 00:45:57 +0200

    General Improvments

--- ask2/approximate.m
+++ ask2/approximate.m
@@ -2,25 +2,18 @@ function approximate(l)
 % APPROXIMATE - 
 %   
 
-    % approximation = 1;
-    % fact = 0;
-    % for n = 1:l
-    %     fact = factorial(n);
-    %     approximation = approximation + 1/fact;
-    % end
-    % fprintf("%0.10f\n", approximation);
-    % fprintf("%0.10f\n", exp(1));
-
-    n = ceil(l / log10(2));
-
     eM = exp(1);
+    e = 0;
+    n = 136;
+    tol = 10^-(l+1)
 
-    e = (1 + 1/n)^n
-    
-    dif = eM - e;
+    while(abs(eM - e) > tol)
+        e = (1 + 1/n )^n;
+        n = n + 1;
+    end
 
-    fprintf("%.11f \n", eM);
-    fprintf("%.11f \n", e);
-    fprintf("%.11f \n", dif);
-
+    format long;
+    eM
+    e
+    n
 end
--- ask6/gramSchmidt.m
+++ ask6/gramSchmidt.m
@@ -23,28 +23,35 @@ function [ U ] = gramSchmidt(V)
 end
 
 function [ res ] = proj(v, u)
-    m = size(v, 1);
+    % m = size(v, 1);
 
-    vecProd = v.*u;
-    vecSq = u.^2;
+    % vecProd = v.*u;
+    % vecSq = u.^2;
 
-    dotProduct = 0;
-    normSquared = 0;
+    dotProduct = (v)'*u;
+    normSquared = (u)'*u;
+
+    % dotProduct = sum(v.*u);
+    % normSquared = sum(u.^2);
+
+    % dotProduct = 0;
+    % normSquared = 0;
+
 
     % for i = 1:m
     %     dotProduct = dotProduct + vecProd(i);
     %     normSquared = normSquared + vecSq(i);
     % end
-    for i = 1:floor(m/2)
-        dotProduct = dotProduct + vecProd(i);
-        dotProduct = dotProduct + vecProd(i+floor(m/2));
-        normSquared = normSquared + vecSq(i);
-        normSquared = normSquared + vecSq(i+floor(m/2));
-    end
-    if(mod(m, 2))
-        dotProduct = dotProduct + vecProd(m);
-        normSquared = normSquared + vecSq(m);
-    end
+    % for i = 1:floor(m/2)
+    %     dotProduct = dotProduct + vecProd(i);
+    %     dotProduct = dotProduct + vecProd(i+floor(m/2));
+    %     normSquared = normSquared + vecSq(i);
+    %     normSquared = normSquared + vecSq(i+floor(m/2));
+    % end
+    % if(mod(m, 2))
+    %     dotProduct = dotProduct + vecProd(m);
+    %     normSquared = normSquared + vecSq(m);
+    % end
 
     res = (dotProduct / normSquared) * u;
 end

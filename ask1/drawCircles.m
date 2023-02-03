65a1c625a9035f5ed9440026681f9453d2512667
TasosFrago <tasosfragkopoulos03@gmail.com>
2023-02-03 03:17:16 +0200

    Better comments to ask1 and ask3

--- ask1/drawCircles.m
+++ ask1/drawCircles.m
@@ -1,35 +1,43 @@
 function drawCircles(x, y)
-    %DRAWCIRCLES Summary of this function goes here
-    %   Detailed explanation goes here
+% DRAWCIRCLES - This function plots three circles that intersect at 3 given points.
+%   The input arguments x and y are the x and y-coordinates of the three points respectively.
+%   The function first calculates the lengths of the sides of the triangle formed by the three points,
+%   using the distance formula. The radii of the three circles are then calculated using the triangle sides.
+%   The function then plots the three sides of the triangle and the three circles using the 'plot' function.
+%   The 'axis equal' command ensures that the x and y scales are equal, so that the circles appear circular.
+%   The 'hold on' and 'hold off' commands ensure that multiple plots are not overwritten.
 
-    AB = sqrt((x(2) - x(1))^2 + (y(2) - y(1))^2);
-    AC = sqrt((x(3) - x(1))^2 + (y(3) - y(1))^2);
-    BC = sqrt((x(3) - x(2))^2 + (y(3) - y(2))^2);
+    AB = sqrt((x(2) - x(1))^2 + (y(2) - y(1))^2); % Length of side AB
+    AC = sqrt((x(3) - x(1))^2 + (y(3) - y(1))^2); % Length of side AC
+    BC = sqrt((x(3) - x(2))^2 + (y(3) - y(2))^2); % Length of side BC
 
-    R = [];
-    R(1) = ( AB + AC - BC)/2;
-    R(2) = ( AB - AC + BC)/2;
-    R(3) = (-AB + AC + BC)/2;
+    R = [];                   % Initialize array to store radii
+    R(1) = ( AB + AC - BC)/2; % Radius of circle centered at first point
+    R(2) = ( AB - AC + BC)/2; % Radius of circle centered at second point
+    R(3) = (-AB + AC + BC)/2; % Radius of circle centered at third point
 
     hold on
 
-    plot([x(1) x(2)], [y(1) y(2)], 'b');
-    plot([x(2) x(3)], [y(2) y(3)], 'b');
-    plot([x(1) x(3)], [y(1) y(3)], 'b');
+    plot([x(1) x(2)], [y(1) y(2)], 'b'); % Plot side AB in blue
+    plot([x(2) x(3)], [y(2) y(3)], 'b'); % Plot side BC in blue
+    plot([x(1) x(3)], [y(1) y(3)], 'b'); % Plot side AC in blue
 
-    circle(x(1), y(1), R(1));
-    circle(x(2), y(2), R(2));
-    circle(x(3), y(3), R(3));
+    circle(x(1), y(1), R(1)); % Plot circle centered at first point
+    circle(x(2), y(2), R(2)); % Plot circle centered at second point
+    circle(x(3), y(3), R(3)); % Plot circle centered at third point
 
     axis equal
     hold off
 end
 
 function circle(x, y, r)
-% CIRCLE - 
-%   
-    th = 0:pi/50:2*pi;
-    xunit = r * cos(th) + x;
-    yunit = r * sin(th) + y;
-    plot(xunit, yunit);
+% CIRCLE - This function plots a circle centered at (x, y) with radius r.
+%   The input arguments x, y and r are the x and y-coordinates of the center of the circle and its radius, respectively.
+%   The function calculates the x and y-coordinates of 50 points evenly spaced around the circumference of the circle,
+%   using the parametric equation of a circle. The function then plots the circle using the 'plot' function.
+
+    th = 0:pi/50:2*pi;         % Theta range for 50 points
+    xunit = r * cos(th) + x;   % x-coordinates of 50 points on circle
+    yunit = r * sin(th) + y;   % y-coordinates of 50 points on circle
+    plot(xunit, yunit);        % Plot the circle
 end
--- ask3/findOccurances.m
+++ ask3/findOccurances.m
@@ -1,18 +1,41 @@
 function findOccurances(str)
-% FINDOCCURANCES - 
-%   
+% FINDOCCURANCES - function to find number of occurrences of each letter in a string
+% 
+% The function takes one input argument `str`, which is the string to be processed.
+% The function outputs the number of occurrences of each letter in the string alphabeticly.
+%
+% Example:
+%     findOccurances('Hello world')
+%
+% Output:
+%     Found d: 1 times 
+%     Found e: 1 times 
+%     Found h: 1 times 
+%     Found l: 3 times 
+%     Found o: 2 times 
+%     Found r: 1 times 
+%     Found w: 1 times 
+%=============================================================================================
+
+    % Create an array `res` with 26 elements, each representing a letter of the alphabet.
+    % The array is initialized to zero.
     res = zeros(1,26);
 
+    % Scan the input string `str`
     for i = 1:strlength(str)
+        % If a character in the string is between ASCII values 65 and 90 (upper case letters),
+        % its count is increased in the `res` array at the corresponding index (`str(i) - 64`).
         if(str(i) >= 65 && str(i) <= 90)
             res(str(i) - 64) = res(str(i) - 64) + 1;
 
+        % If a character is between ASCII values 97 and 122 (lower case letters),
+        % its count is increased in the `res` array at the corresponding index (`str(i) - 96`).
         elseif (str(i) >= 97 && str(i) <= 122)
             res(str(i) - 96) = res(str(i) - 96) + 1;
-
         end
     end
 
+    % Output the number of occurrences of each letter in the string
     for i = 1:26
         if(res(i) ~= 0)
             fprintf("Found %c: %d times \n", (i+96), res(i));

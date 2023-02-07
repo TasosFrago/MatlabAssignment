b4470849986a869b2d3f2c803b5c26b22b142fb3
TasosFrago <tasosfragkopoulos03@gmail.com>
2023-02-07 02:32:38 +0200

    Added ask7 to README

--- ask7/newtonRaphson.m
+++ ask7/newtonRaphson.m
@@ -1,16 +1,47 @@
 function [res, i] = newtonRaphson(f, df, x0, n, tol)
+% NEWTONRAPHSON - Finds the roots of a system of non-linear equations using the Newton-Raphson method
+% Inputs:
+% f	- function handle representing the system of non-linear equations
+% df	- function handle representing the Jacobian matrix of f
+% x0	- initial guess for the solution
+% n	- maximum number of iterations to perform
+% tol	- tolerance for the stopping criteria
+%
+% Outputs:
+% res	- estimated solution to the system of non-linear equations
+% i	- number of iterations performed
+
+    % Initialize result
     res = [0 0];
+
+    % Perform Newton-Raphson iteration
     for i = 1:n
-        invJ = inv(df(x0));
+        % Compute the inverse Jacobian and the residual of the system
+        % invJ = inv(df(x0));
+        invJ = inverse(df(x0));
         F = f(x0);
         res = x0 - (invJ*F)';
 
+        % Check if the solution has converged
         if(abs(res - x0) < tol)
-            break
+            break; % Solution has converged
         end
+
+        % Update the guess for the next iteration
         x0 = res;
     end
+    % Check if the Newton-Raphson method failed to converge
     if(i == n)
         fprintf("Newton-Raphson method failed to converge.\n");
     end
+end
+
+function [B] = inverse(A)
+% INVERSE - Find the inverse of a 2x2 matrix
+% A: 2x2 matrix to find the inverse of
+% B: Inverse of A
+
+    detA = A(1, 1)*A(2, 2) - A(1, 2)*A(2, 1);	% Find the determinant of A
+    adjA = [A(2, 2) -A(1, 2); -A(2, 1) A(1, 1)];% Find the adjugate of A
+    B = (1/detA) * adjA;			% Divide the adjugate of A by its determinant to get the inverse of A
 end
--- ask7/run.m
+++ ask7/run.m
@@ -1,30 +1,34 @@
 clear
 clc
 
+% Matix with functions f and g inside
 f = @(x) [
-    1-x(1)-x(2)*sin(pi*x(1)/2);  % f(x, y)
-    exp(-x(2))+10*cos(pi*x(1))-2 % g(x, y)
+    1-x(1)-x(2)*sin(pi*x(1)/2);   % f(x, y) - First equation
+    exp(-x(2))+10*cos(pi*x(1))-2  % g(x, y) - Second equation
          ];
 
+% Jacobian matrix of matrix f
 df = @(x) [
     -1-x(2)*cos(pi*x(1)/2)*(pi/2),   -sin(pi*x(1)/2);
     -10*pi*sin(pi*x(1)),             -exp(-x(2))
           ];
 
-tol = 1e-7;
+tol = 1e-7; % Tolerance value for the Newton-Raphson method
 
-x0 = [0.1 0.1]; n = 20;
+% Different initial guess values and number of iterations.
+x0 = [-1 0]; n = 16;		% res --> [1.499230 -0.705166]
+% x0 = [0.1 0.1]; n = 20;	% res --> [1.499230 -0.705166]
+% x0 = [1 0]; n = 18;		% res --> [14.435906 21.244032]
 
-% x0 = [1 0]; n = 18;
+[res, times] = newtonRaphson(f, df, x0, n, tol); % Call to the Newton-Raphson method
 
-% x0 = [-1 0]; n = 15;
+ti = @() newtonRaphson(f, df, x0, n, tol);
+time = timeit(ti);
 
-% x0 = [-0.1 0]; n = 30;
+fres = f(res); % Evaluate the function at the obtained result
 
-[res, times] = newtonRaphson(f, df, x0, n, tol);
-
-fres = f(res);
+% Display the result and the number of iterations.
 fprintf("For f(x, y) = 0 and g(x, y) = 0 using the method Newton-Raphson that run %d times, we found that\n\tx0 = %.6f,\n\ty0 = %.6f\n", times, res(1), res(2));
 fprintf("f(x0, y0) = %.6f\ng(x0, y0) = %.6f\n", fres(1), fres(2));
 
-% x0 = [14 21];
+fprintf("The function took %fs to run.\n", time);
